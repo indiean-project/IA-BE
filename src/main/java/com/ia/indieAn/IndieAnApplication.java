f0426175a6d95a7d50f431d29be442f136466317
comet <comet2667@naver.com>
2024-04-28 14:40:15 +0900

    프로젝트 생성

---
+++ src/main/java/com/ia/indieAn/IndieAnApplication.java
@@ -0,0 +1,13 @@
+package com.ia.indieAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndieAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndieAnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ia/indieAn/IndieAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.indieAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndieAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
