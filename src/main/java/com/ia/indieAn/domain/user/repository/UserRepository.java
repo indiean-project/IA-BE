47c0bff0cb56e05a03f016de392f1dcfbed2e6c9
comet <comet2667@naver.com>
2024-05-10 17:15:29 +0900

    병합 오류 수정

--- src/main/java/com/ia/indieAn/domain/user/repository/UserRepository.java
+++ src/main/java/com/ia/indieAn/domain/user/repository/UserRepository.java
@@ -12,6 +12,4 @@ public interface UserRepository extends JpaRepository<Member, Integer> {
     boolean existsByUserId(String userId);
     boolean existsByNickname(String nickname);
     boolean existsByPhone(String phone);
-
-    Member findByUserNo(int userNo);
 }
