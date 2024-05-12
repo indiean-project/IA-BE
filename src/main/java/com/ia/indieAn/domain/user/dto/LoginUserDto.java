564a2ebfc2522287f86d3aa8d567ff666ccaff9b
Ojlee <ojlee6207@gmail.com>
2024-05-12 22:28:53 +0900

    240512 member 관련 dto 수정

--- src/main/java/com/ia/indieAn/domain/user/dto/LoginUserDto.java
+++ src/main/java/com/ia/indieAn/domain/user/dto/LoginUserDto.java
@@ -1,6 +1,7 @@
 package com.ia.indieAn.domain.user.dto;
 
 import com.ia.indieAn.entity.user.Member;
+import jakarta.persistence.Column;
 import lombok.Data;
 
 @Data
@@ -8,18 +9,32 @@ public class LoginUserDto {
 
     private int userNo;
     private String userId;
+    private String userPwd;
     private String userName;
     private String nickname;
     private String phone;
     private String address;
+    private String deleteYn;
+    private String reportStatus;
+    private String userProfileImg;
+    private String userContent;
+    private String userFavoriteArtist;
+    private String userFavoriteMusic;
 
     public LoginUserDto(Member member){
         this.userNo = member.getUserNo();
         this.userId = member.getUserId();
+        this.userPwd = member.getUserPwd();
         this.userName = member.getUserName();
         this.nickname = member.getNickname();
         this.phone = member.getPhone();
         this.address = member.getAddress();
+        this.deleteYn = member.getDeleteYn();
+        this.reportStatus = member.getReportStatus();
+        this.userProfileImg = member.getUserProfileImg();
+        this.userContent = member.getUserContent();
+        this.userFavoriteArtist = member.getUserFavoriteArtist();
+        this.userFavoriteMusic = member.getUserFavoriteMusic();
     }
 
 }
