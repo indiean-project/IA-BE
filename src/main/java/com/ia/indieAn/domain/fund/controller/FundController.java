b633c49ff4d34d58dfc6b9a70f3014bddd2a6cf0
comet <comet2667@naver.com>
2024-05-17 01:32:13 +0900

    결제 연동 준비

--- src/main/java/com/ia/indieAn/domain/fund/controller/FundController.java
+++ src/main/java/com/ia/indieAn/domain/fund/controller/FundController.java
@@ -6,7 +6,11 @@ import com.ia.indieAn.domain.fund.dto.FundDetailDto;
 import com.ia.indieAn.domain.fund.dto.FundListDto;
 import com.ia.indieAn.domain.fund.dto.FundSearchDto;
 import com.ia.indieAn.domain.fund.service.FundService;
+import kr.co.bootpay.Bootpay;
+import kr.co.bootpay.model.request.SubscribePayload;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,7 +19,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.TimeZone;
 
 @RestController
 @RequestMapping("/api/fund")
@@ -24,6 +32,12 @@ public class FundController {
 
     @Autowired
     FundService fundService;
+
+    @Value("${bootPay.rest-application-id}")
+    private String bootPay_key;
+
+    @Value("${bootPay.private-key}")
+    private String private_key;
 
     @RequestMapping("/allList")
     public ResponseEntity<ResponseTemplate> selectAllFund(@RequestBody FundSearchDto fundSearchDto){
@@ -69,6 +83,57 @@ public class FundController {
         FundDetailDto fundDetailDto = fundService.selectFundDetail(fundNo);
         response.setStatus(StatusEnum.SUCCESS);
         response.setData(fundDetailDto);
+
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
+    }
+
+    @RequestMapping("/order")
+    public ResponseEntity<ResponseTemplate> orderReward() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        ResponseTemplate response = new ResponseTemplate();
+
+        Bootpay bootpay = new Bootpay(bootPay_key, private_key);
+        bootpay.getAccessToken();
+        HashMap res = null;
+        try {
+            res = bootpay.lookupBillingKey("ReceipId//변경예정");
+            JSONObject json = new JSONObject(res);
+            if (res.get("error_code") == null){
+                System.out.println("성공" + res);
+            } else {
+                System.out.println("실패" + res);
+                return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        SubscribePayload payload = new SubscribePayload();
+        payload.billingKey = (String)res.get("billing_key");
+        payload.orderName = "펀딩";
+        payload.price = 1000;
+        payload.orderId = "123456";
+
+        Date now = new Date();
+        now.setTime(now.getTime() + 20 * 1000); //20초 뒤 결제
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss XXX");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        payload.reserveExecuteAt = sdf.format(now);
+
+        try {
+            HashMap postRes = bootpay.reserveSubscribe(payload);
+            JSONObject postJson = new JSONObject(postRes);
+            System.out.println("예약결제" + postJson);
+
+            if (postRes.get("error_code") == null){
+                System.out.println("예약결제 성공" + postRes);
+            } else {
+                System.out.println("예약결제 실패" + postRes);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
         return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
