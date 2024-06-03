a472a58069154476122e374bbfe0091056eb86c0
leedonghyeob <m715800@naver.com>
2024-06-03 13:53:54 +0900

    신고 관련 서칭 기능 오류 + 유저권한 작업 중

--- src/main/java/com/ia/indieAn/admin/user/Authority/service/AuthorityAdminService.java
+++ src/main/java/com/ia/indieAn/admin/user/Authority/service/AuthorityAdminService.java
@@ -4,6 +4,9 @@ import com.ia.indieAn.admin.user.Authority.dto.AuthorityAdminDto;
 import com.ia.indieAn.admin.user.Authority.repository.AuthorityAdminRepository;
 import com.ia.indieAn.domain.user.repository.UserRepository;
 import com.ia.indieAn.entity.artist.Artist;
+import com.ia.indieAn.entity.user.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +15,7 @@ import java.util.ArrayList;
 
 @Service
 @Transactional(readOnly=true)
+@Slf4j
 public class AuthorityAdminService {
 
     @Autowired
@@ -21,50 +25,49 @@ public class AuthorityAdminService {
     UserRepository userRepository;
     public ArrayList<AuthorityAdminDto> selectAllAuthorityList(){
         ArrayList<Artist> authorityList = (ArrayList<Artist>)authorityAdminRepository.findAll();
-//        System.out.println(authorityList);
+        log.info("size{}", authorityList.size());
 
         ArrayList<AuthorityAdminDto> authorityAdminDtoArrayList = new ArrayList<>();
         for(int i=0; i<authorityList.size(); i++){
-//            Member m = userRepository.findByUserNo(authorityList.get(i).getMember().getUserNo());
             authorityAdminDtoArrayList.add(new AuthorityAdminDto(authorityList.get(i)));
         }
-//        System.out.println(authorityAdminDtoArrayList);
+
         return authorityAdminDtoArrayList;
 
     }
 
-    public ArrayList<AuthorityAdminDto> selectAllAuthorityTitle(){
-        ArrayList<Artist> authorityList = (ArrayList<Artist>)authorityAdminRepository.findAll();
-
-        ArrayList<AuthorityAdminDto> authorityAdminDtoAuthorityTitle = new ArrayList<>();
-        for(Artist artist : authorityList){
-            authorityAdminDtoAuthorityTitle.add(new AuthorityAdminDto(artist));
-        }
-        return authorityAdminDtoAuthorityTitle;
-
-    }
-
-    public ArrayList<AuthorityAdminDto> selectAllAuthorityStatus(){
-        ArrayList<Artist> authorityList = (ArrayList<Artist>)authorityAdminRepository.findAll();
-
-        ArrayList<AuthorityAdminDto> authorityAdminDtoAuthorityStatus = new ArrayList<>();
-        for(Artist artist : authorityList){
-            authorityAdminDtoAuthorityStatus.add(new AuthorityAdminDto(artist));
-        }
-        return authorityAdminDtoAuthorityStatus;
-
-
-    }
-
-    public ArrayList<AuthorityAdminDto> selectAllAuthorityType(){
-        ArrayList<Artist> authorityList = (ArrayList<Artist>)authorityAdminRepository.findAll();
-
-        ArrayList<AuthorityAdminDto> authorityAdminDtoAuthorityType = new ArrayList<>();
-        for(Artist artist : authorityList){
-            authorityAdminDtoAuthorityType.add(new AuthorityAdminDto(artist));
-        }
-        return authorityAdminDtoAuthorityType;
-
-    }
+//    public ArrayList<AuthorityAdminDto> selectAllAuthorityTitle(){
+//        ArrayList<Artist> authorityList = (ArrayList<Artist>)authorityAdminRepository.findAll();
+//
+//        ArrayList<AuthorityAdminDto> authorityAdminDtoAuthorityTitle = new ArrayList<>();
+//        for(Artist artist : authorityList){
+//            authorityAdminDtoAuthorityTitle.add(new AuthorityAdminDto(artist));
+//        }
+//        return authorityAdminDtoAuthorityTitle;
+//
+//    }
+//
+//    public ArrayList<AuthorityAdminDto> selectAllAuthorityStatus(){
+//        ArrayList<Artist> authorityList = (ArrayList<Artist>)authorityAdminRepository.findAll();
+//
+//        ArrayList<AuthorityAdminDto> authorityAdminDtoAuthorityStatus = new ArrayList<>();
+//        for(Artist artist : authorityList){
+//            authorityAdminDtoAuthorityStatus.add(new AuthorityAdminDto(artist));
+//        }
+//        return authorityAdminDtoAuthorityStatus;
+//
+//
+//    }
+//
+//    public ArrayList<AuthorityAdminDto> selectAllAuthorityType(){
+//        ArrayList<Artist> authorityList = (ArrayList<Artist>)authorityAdminRepository.findAll();
+//
+//        ArrayList<AuthorityAdminDto> authorityAdminDtoAuthorityType = new ArrayList<>();
+//        for(Artist artist : authorityList){
+//            authorityAdminDtoAuthorityType.add(new AuthorityAdminDto(artist));
+//        }
+//        return authorityAdminDtoAuthorityType;
+//
+//    }
 
 }
--- src/main/java/com/ia/indieAn/admin/user/Question/repository/QuestionAdminRepository.java
+++ src/main/java/com/ia/indieAn/admin/user/Question/repository/QuestionAdminRepository.java
@@ -3,6 +3,7 @@ package com.ia.indieAn.admin.user.Question.repository;
 
 import com.ia.indieAn.entity.user.Member;
 import com.ia.indieAn.entity.user.Question;
+import com.ia.indieAn.type.enumType.BrcTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
@@ -15,5 +16,6 @@ public interface QuestionAdminRepository extends JpaRepository<Question, Integer
  Optional<Question> findByQuestionNo(int questionNo);
  ArrayList<Question> findByQuestionContentContaining(String questionContent);
  ArrayList<Question> findByMember_UserNo(int userNo);
+
 
 }
